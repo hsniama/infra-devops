a51f0acebdfa603eff0935d954f5c71c5e5a40b4
Henry Niama <48260750+hsniama@users.noreply.github.com>
2026-01-11 11:04:53 -0500

    Fix: errors deploying with terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "node_count" {
 
 variable "node_vm_size" {
   type    = string
-  default = "Standard_B2pls_v2"
+  default = "Standard_D2s_v3"
 }
 
 variable "vnet_name" { type = string }
