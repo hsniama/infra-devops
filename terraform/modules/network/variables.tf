4aadc4c244f8db4a455388357a3a6cb3cb4428d7
Henry Niama <48260750+hsniama@users.noreply.github.com>
2026-01-26 10:32:54 -0500

    1. Refactorizando modulo Network

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,27 @@
+variable "vnet_name" { 
+  type = string
+}
+
+variable "location" {
+    type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_cidr" {
+    type = string
+}
+
+variable "tags" {
+  type    = map(string)
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_cidr" {
+    type = string
+}
