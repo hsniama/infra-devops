c6ea0680098d1ece1f4683665cb80a6f9c160bba
Henry Niama <48260750+hsniama@users.noreply.github.com>
2026-01-26 11:18:13 -0500

    1. Refactorizando modulo ACR

---
+++ terraform/modules/acr/outputs.tf
@@ -0,0 +1,14 @@
+output "acr_id" {
+  description = "ID del Azure Container Registry"
+  value       = azurerm_container_registry.this.id
+}
+
+output "acr_name" {
+  description = "Nombre del Azure Container Registry"
+  value = azurerm_container_registry.this.name
+}
+
+output "login_server" {
+    description = "Login server del Azure Container Registry"
+    value       = azurerm_container_registry.this.login_server
+}
